1370dcddbd2a601e2278d6cd5c01e6609418c047
jonathan <qf.jonathan@gmail.com>
2012-05-18 01:28:51 -0500

    algunas funciones para el controlador base

--- base/controlador.php
+++ base/controlador.php
@@ -5,6 +5,18 @@ class Controlador {
 	public function __construct() {
 		;
 	}
+	
+	public function cargar_vista($vista){
+		
+	}
+	
+	public function cargar_modelo($vista){
+		
+	}
+	
+	public function cargar_libreria($libreria){
+		
+	}
 
 }
 
